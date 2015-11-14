ca93eb6eb818ab903003089736ce13cbe2377ad5
Matthew Dowling <Matthew Dowling>
2015-11-14 15:35:09 -0500

    fuck git

---
+++ app/src/main/java/com/acme/audioplaybackinterface/RadioREcord.java
@@ -0,0 +1,78 @@
+package com.acme.audioplaybackinterface;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+/**
+ * Created by Matthew on 10/22/2015.
+ */
+public class RadioREcord implements Runnable {
+    public static int DEFAULT_SAMPLE_RATE = 44100;
+    public static int DEFAULT_BUFFER_SIZE = 1024;
+    private boolean recording;
+    //private AudioTrack audioTrack;
+    private AudioRecord audioRecord;
+    short[] buffer;
+    int minBufferSize;
+    int bufferSize;
+
+    RadioREcord() {
+        this.recording = true;
+
+
+
+        buffer = new short[DEFAULT_BUFFER_SIZE];
+    }
+
+    public void run() {
+
+
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, DEFAULT_SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, DEFAULT_BUFFER_SIZE * 1);
+
+       // audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+        //        DEFAULT_SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+       //         AudioFormat.ENCODING_PCM_16BIT, DEFAULT_BUFFER_SIZE * 1,
+       //         AudioTrack.MODE_STREAM);
+
+       // audioTrack.setPlaybackRate(DEFAULT_SAMPLE_RATE);
+
+
+        audioRecord.startRecording();
+        //Log.i(LOG_TAG,"Audio Recording started");
+        //audioTrack.play();
+        //Log.i(LOG_TAG,"Audio Playing started");
+        while (recording) {
+            audioRecord.read(buffer, 0, DEFAULT_BUFFER_SIZE);
+           // audioTrack.write(buffer, 0, buffer.length);
+
+        }
+        audioRecord.stop();
+        audioRecord.release();
+        audioRecord = null;
+       // audioTrack.stop();
+       // audioTrack.release();
+       // audioTrack = null;
+
+    }
+
+    public void stopRecord(){
+        this.recording = false;
+    }
+
+
+    /**ToDo:
+     * Fix Audio Routing - force to speakers/hdmi (remove C-MEDIA sink from usbaudio.conf?
+     * Force Input Routing - on device (like Note 4) with internal MIC, force input to USB mic
+     * Detect USB Device - auto launch application when USB device with given PID/VID is attached
+     * Set Up RTP streaming, create service which continues to run in background even after App closed
+     * Setup to record to file for set amount of time
+     * Add pause/rewind/delayed time playback controls
+     * Add visual audio playback indicator (where in buffered audio are we playing)
+     * Check Audio quality / make sure settings for buffer size / sample rate are correct
+     * Push buffer size/ sample rate/ etc to GUI for customizability (maybe fetch from device then populate options)
+     * Setup GIT repo (integrated with Android Studio?)
+     * EXTENDED TIME!!!!
+     */
+
+}
